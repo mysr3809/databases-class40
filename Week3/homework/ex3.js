98e83af4f7bdf599f68bf21e1715da59ed8e6017
mysr3809 <myasar0938@gmail.com>
2022-12-13 14:41:40 +0100

    added some solution

--- Week3/homework/ex3.js
+++ Week3/homework/ex3.js
@@ -20,12 +20,14 @@ const con = createConnection({
 //   con.end();
 // };
 
+
+//first way to avoid sql injections to use escape method
 function getPopulation(Country, name, code, cb) {
-  name = con.escape(name); // to avoid sql injection, used escape method
+  name = con.escape(name);
   code = con.escape(code);
   console.log("name", name, code);
   con.query(
-    `SELECT Population FROM ${Country} WHERE Name = ${name} and code =${code}`,
+    `SELECT Population FROM ${Country} WHERE Name = '?' and code = '?'`,
     function (err, result) {
       if (err) cb(err);
       if (result.length == 0) cb(new Error("Not found"));
@@ -34,6 +36,20 @@ function getPopulation(Country, name, code, cb) {
   );
   con.end();
 };
+
+//second way to use question mark in query
+// function getPopulation(Country, name, code, cb) {
+//   console.log("name", name, code);
+//   con.query(
+//     `SELECT Population FROM ${Country} WHERE Name = '?' and code = '?'`,
+//     function (err, result) {
+//       if (err) cb(err);
+//       if (result.length == 0) cb(new Error("Not found"));
+//       cb(null, result);
+//     }
+//   );
+//   con.end();
+// };
 
 getPopulation("Country", "Turkey", "TUR 'OR' 1=1;", (error, result) => { // this values give us whole data for the first function
   if (error) {
